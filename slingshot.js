b421247fb3e1d71c95e7002dff3684d4ebd9d776
PARAS134 <76866967+PARAS134@users.noreply.github.com>
2021-04-01 14:39:29 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,25 @@
+class slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB=pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    fly(){
+        //when mouse is release we are not attaching any bird to sling that's why we have written null ;
+      this.sling.bodyA=null
+    }
+    display(){
+        if (this.sling.bodyA!==null){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+     }
+}
